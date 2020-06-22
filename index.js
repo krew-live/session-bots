21effde691b9b7ab037bf23d6f0d54dcf0f08466
Yousef Amar <yousefamar@gmail.com>
2020-06-22 23:18:35 +0100

    Change URL and add no sandbox

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 (async () => {
 	const browser = await puppeteer.launch({
 		args: [
+			'--no-sandbox',
 			'--use-fake-ui-for-media-stream',
 			'--use-fake-device-for-media-stream',
 			'--use-file-for-fake-video-capture=' + __dirname + '/webcam.mjpeg',
@@ -15,8 +16,8 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 		const page = await browser.newPage();
 		//page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 		page.on('pageerror', e => console.log(message));
-		await page.goto('http://localhost:8080/session/bots');
-		//await page.goto('https://krew.live/session/bots');
+		//await page.goto('http://localhost:8080/session/bots');
+		await page.goto('https://krew.live/session/bots');
 		await page.click('#camButton');
 		await sleep(1000);
 		await page.click('#joinButton');
